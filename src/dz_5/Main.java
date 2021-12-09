031b79f0a17638f497e2d2d9db70dd21c1e7a9ca
Aleksey Astakhov <astahov.2004@ukr.net>
2021-12-09 23:07:07 +0200

    Did sixth homework

--- src/dz_5/Main.java
+++ src/dz_5/Main.java
@@ -1,9 +1,16 @@
 package dz_5;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         Auto automaticCar = new Automatic(123, 123, 123, "S", "S");
         Auto mechanicalCar = new Mechanical(123, 123, 123, "S", "S");
+
+        LinkedList<String> list = new LinkedList<>();
+        List<Integer> list1 = new LinkedList<>();
+
 
         checkingClass(automaticCar);
         checkingClass(mechanicalCar);
